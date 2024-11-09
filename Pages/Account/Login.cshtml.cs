69a0f99b62ec24e492d416694cdb136da8d344d1
Williamvilla25 <williamvilla727@gmail.com>
2024-11-09 12:53:04 -0500

    autenticación carpeta pages

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Security.Claims;
+
+namespace SupermarketWEB.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+
+        private readonly SupermarketContext _context;
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public LoginModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            // Busca un usuario con el correo electrónico especificado
+            var user = await _context.Acounts.FirstOrDefaultAsync(u => u.Email == User.Email);
+
+            if (user != null && user.Password == User.Password)
+            {
+                var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "admin"),
+                new Claim(ClaimTypes.Email, User.Email),
+            };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/index");
+            }
+
+            // Usuario no encontrado o contraseña incorrecta
+            ModelState.AddModelError(string.Empty, "Correo electrónico o contraseña incorrectos.");
+            return Page();
+        }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public IndexModel(SupermarketContext context)
+        { 
+            _context = context;
+        }
+        public IList<Category> Categories { get; set; }
+        public async Task OnGetAsync()
+        {
+            //if (_context.Categories != null)
+            //{
+                Categories = await _context.Categories.ToListAsync();
+            //}
+        }
+        
+    }
+}
